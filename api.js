85f532c8673e1ca421a9a6567cb653abed6e425d
Matt Moore <matt@cloudkid.com>
2013-11-18 15:16:22 -0500

    Auto updated docs

---
+++ api.js
@@ -0,0 +1,26 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "ImageResizer",
+        "ImageUploader",
+        "MediaUtils",
+        "ResizerError",
+        "Uploader",
+        "UploaderError"
+    ],
+    "modules": [
+        "Canteen_Errors",
+        "Canteen_Media"
+    ],
+    "allModules": [
+        {
+            "displayName": "Canteen\\Errors",
+            "name": "Canteen_Errors"
+        },
+        {
+            "displayName": "Canteen\\Media",
+            "name": "Canteen_Media"
+        }
+    ]
+} };
+});

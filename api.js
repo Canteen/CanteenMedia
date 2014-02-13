81605dad82bbf1ce102d5ee0f7e1a5e1bea1cc85
Matt Karl <matt@cloudkid.com>
2014-02-13 14:04:54 -0500

    Auto updated docs

--- api.js
+++ api.js
@@ -2,21 +2,16 @@ YUI.add("yuidoc-meta", function(Y) {
    Y.YUIDoc = { meta: {
     "classes": [
         "ImageResizer",
+        "ImageResizerError",
         "ImageUploader",
         "MediaUtils",
-        "ResizerError",
         "Uploader",
         "UploaderError"
     ],
     "modules": [
-        "Canteen_Errors",
         "Canteen_Media"
     ],
     "allModules": [
-        {
-            "displayName": "Canteen\\Errors",
-            "name": "Canteen_Errors"
-        },
         {
             "displayName": "Canteen\\Media",
             "name": "Canteen_Media"
